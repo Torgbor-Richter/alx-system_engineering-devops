35df1c3e31f3c5c7d17c75c0489d41ed415d39a0
Torgbor-Richter <torgborrishter23@gmail.com>
2022-12-21 09:53:11 +0000

    config

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# Script Creates a file in /tmp
+
+file { 'school':
+  ensure  => 'present',
+  content => 'I love Puppet',
+  mode    => '0744',
+  group   => 'www-data',
+  owner   => 'www-data',
+  path    => '/tmp/school',
+}
